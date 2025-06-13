35c70b76a608d1be6bd3265ad6ec8e4ab5e6c918
DiegoGalantee <diegogalante07@gmail.com>
2025-06-13 19:31:31 +0000

    completado con boton de cambiar, agrgar color y reiniciar

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import "../../styles/index.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const TrafficLight = () => {
+    const [color, setColor] = useState("red");
+    const [colors, setColors] = useState(["red", "yellow", "green"]);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+    const handleClick = (selectedColor) => {
+        setColor(selectedColor);
+    };
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const cycleColor = () => {
+        const index = colors.indexOf(color);
+        const nextColor = colors[(index + 1) % colors.length];
+        setColor(nextColor);
+    };
+
+    const generateRandomColor = () => {
+        const randomColor = "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, "0");
+        return randomColor;
+    };
+
+    const addRandomColor = () => {
+        let newColor;
+        do {
+            newColor = generateRandomColor();
+        } while (colors.includes(newColor));
+        setColors([...colors, newColor]);
+    };
+
+    const resetColors = () => {
+        setColors(["red", "yellow", "green"]);
+        setColor("red");
+    };
+
+    return (
+        <div className="text-center mt-5">
+            <div className="traffic-light">
+                {colors.map((c) => (
+                    <div
+                        key={c}
+                        onClick={() => handleClick(c)}
+                        className={`light ${color === c ? "glow" : ""}`}
+                        style={{ backgroundColor: c }}
+                    ></div>
+                ))}
+            </div>
+            <button className="btn btn-primary m-2" onClick={cycleColor}>
+                Cambiar Color
+            </button>
+            <button className="btn btn-secondary m-2" onClick={addRandomColor}>
+                Agregar color
+            </button>
+            <button className="btn btn-danger m-2" onClick={resetColors}>
+                Reiniciar semáforo
+            </button>
+        </div>
+    );
 };
 
-export default Home;
+export default TrafficLight;
